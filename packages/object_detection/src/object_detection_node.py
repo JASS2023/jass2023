ac3337c8f9dae5bbd5381da7725453ddcd68317b
Ivan Vybornov <vanerk4@gmail.com>
2023-03-29 12:52:48 +0300

    send the biggest detected object back

--- packages/object_detection/src/object_detection_node.py
+++ packages/object_detection/src/object_detection_node.py
@@ -6,20 +6,15 @@ import pickle
 from time import perf_counter
 import requests
 
-# import torch
-# from PIL import Image 
-
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
-from sensor_msgs.msg import CompressedImage
-
+from sensor_msgs.msg import CompressedImage, Float32MultiArray
+import json
 
 COUNTER_FREQUENCY = 10
 
 IP = '192.168.0.176'
 PORT = 8000
 
-# clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# clientSocket.connect((IP, PORT))
 
 class ObjectDetectionNode(DTROS):
     def __init__(self) -> None:
@@ -28,42 +23,44 @@ class ObjectDetectionNode(DTROS):
             node_name="object_detection_node", node_type=NodeType.PERCEPTION
         )
 
-        # self._model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'model.pt')
-        # self._model.load_state_dict(torch.load('model.pt')['model'].state_dict())
-
         self._img_sub = rospy.Subscriber(
             "~image", CompressedImage, self.cb_image, queue_size=1, buff_size="20MB"
         )
 
-        # self._detected_objs = rospy.Publisher(
-        #     "~objects_detected", String, queue_size=1, dt_topic_type=TopicType.DRIVER
-        # )
+        self._detected_objs = rospy.Publisher(
+            "~objects_detected", Float32MultiArray, queue_size=1, dt_topic_type=TopicType.DRIVER
+        )
+        
 
-    def cb_image(self, msg) -> None:
-        # def sort_func(tensor: torch.Tensor) -> float:
-        #     x1, y1, x2, y2, _, _ = tensor
-        #     return (x2 - x1) * (y2 - y1)
-        
-        
-        # dataFromServer = clientSocket.recv(1024)
-        # print(dataFromServer.decode())
-        
+    def cb_image(self, msg) -> None:        
         self.counter += 1
         self.log(f"Received {self.counter} images")
         if self.counter == COUNTER_FREQUENCY:
+            self.counter = 0
+            
             # Capture start time
             t_start = perf_counter()
+            
             send_package = pickle.dumps(msg.data)
             self.log(f"Sending package of size {len(send_package)}")
+        
             # Send the image to the server and receive the response
             response = requests.post(f"http://{IP}:{PORT}/detect_objects", data=send_package)
-            self.counter = 0
+            response = json.loads(response)
+
+            if response is None:
+                return
+
             # Capture end time
             t_end = perf_counter()
+
             # Not sure what kind of objects will be sent (if it is an array of integers, it will be awful)
             self.log(f"Received response: {response} after {t_end - t_start} seconds")
             
-            return
+            # mb receive "the most dangerous" object
+            reply = Float32MultiArray()
+            reply.data = response
+            self._detected_objs.publish(reply)
 
 
 if __name__ == "__main__":
