b096b5d8bb122765d12701c76b30b42ad851cd75
Ilia <lelikk2002@gmail.com>
2023-03-30 10:58:15 +0300

    Initial commit

--- packages/led_signal/src/led_signal_node.py
+++ packages/led_signal/src/led_signal_node.py
@@ -17,43 +17,47 @@ class LEDSignalNode(DTROS):
     def __init__(self, node_name):
         # Initialize the DTROS parent class
         super(LEDSignalNode, self).__init__(node_name=node_name, node_type=NodeType.DRIVER)
-        self.LEDspattern = [[0.0, 0.0, 1.0]] * 5
+        self.LEDsPattern = [[0.0, 0.0, 1.0]] * 5
         self.pub_leds = rospy.Publisher(
             "~led_pattern", LEDPattern, queue_size=1, dt_topic_type=TopicType.DRIVER
         )
-        # self._img_sub = rospy.Subscriber(
-        #     "~image", CompressedImage, self.cb_image, queue_size=1, buff_size="20MB"
-        # )
-        self.april_tags_sub = rospy.Subscriber("~construction_ap_tag", Int32MultiArray, self.onDetectAprilTag, queue_size=1)
-        self.traffic_light_atags_sub = rospy.Subscriber("~traffic_light_ap_tag", std_msgs.msg.String, self.onDetectedTrafficLight, queue_size=1)
-        # traffic_light_ap_tag
-        # self.publishLEDs()
 
-    def onDetectedTrafficLight(self, message):
+        self.april_tags_sub = rospy.Subscriber("~construction_ap_tag", Int32MultiArray, self.on_detect_construction_sign, queue_size=1)
+        self.traffic_light_atags_sub = rospy.Subscriber("~traffic_light_ap_tag", std_msgs.msg.String, self.on_detect_traffic_light, queue_size=1)
+
+    def on_detect_traffic_light(self, message):
         if message.data == "red":
-            self.changeColor([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 0.0, 0.0], [1.0, 0.0, 0.0]])
+            self.change_color([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 0.0, 0.0]])
         else:
-            self.changeColor([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
+            self.change_color([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 0.0, 1.0]])
 
-    def onDetectAprilTag(self, message):
-        self.changeColor([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 0.0, 0.0], [1.0, 0.0, 0.0]])
-        self.changeColor([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
-        # self.LEDspattern = [[0.0, 1.0, 0.0]] * 5
-        # self.publishLEDs()
-        # self.LEDspattern = [[0.0, 0.0, 1.0]] * 5
-        # self.publishLEDs()
-    def changeColor(self, led_pattern):
-        self.LEDspattern = led_pattern
-        self.publishLEDs()
+    def on_detect_construction_sign(self, message):
+        self.change_color([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 0.0, 0.0]])
+        self.change_color([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 0.0, 1.0]])
 
-    def publishLEDs(self):
+    def change_color(self, led_pattern):
+        '''
+
+        Args:
+            led_pattern: - pattern for LEDs, list of 5 lists with RGB, 4th and 5th items are for the back LEDs
+
+        Returns:
+
+        '''
+        self.LEDsPattern = led_pattern
+        self.publish_led()
+
+    def publish_led(self):
+        """
+        Publishes new colors for LEDs into /{bot_name}/led_emitter/led_pattern topic
+        """
         LEDPattern_msg = LEDPattern()
         for k in range(1):
             for i in range(5):
                 rgba = ColorRGBA()
-                rgba.r = self.LEDspattern[i][0]
-                rgba.g = self.LEDspattern[i][1]
-                rgba.b = self.LEDspattern[i][2]
+                rgba.r = self.LEDsPattern[i][0]
+                rgba.g = self.LEDsPattern[i][1]
+                rgba.b = self.LEDsPattern[i][2]
                 rgba.a = 1.0
                 LEDPattern_msg.rgb_vals.append(rgba)
                 self.pub_leds.publish(LEDPattern_msg)
