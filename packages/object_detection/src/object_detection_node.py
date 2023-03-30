a81dd8be4acd62c286ef30f42a9b9fa6b4ee8a19
Ilia <lelikk2002@gmail.com>
2023-03-30 16:21:15 +0300

    Fixed version

--- packages/object_detection/src/object_detection_node.py
+++ packages/object_detection/src/object_detection_node.py
@@ -10,12 +10,13 @@ from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
 from sensor_msgs.msg import CompressedImage
 from std_msgs.msg import Float32MultiArray, String
 
+import os
 
 import json
 
 COUNTER_FREQUENCY = 5
 
-IP = '192.168.0.43'
+IP = '192.168.0.13'
 PORT = 8080
 
 
@@ -72,15 +73,48 @@ class ObjectDetectionNode(DTROS):
             # mb receive "the most dangerous" object
             
             reply = String()
-            obstacle_info = {}
-            obstacle_info["message"] = "discover_obstacle"
-            obstacle_info["id"] = -1
-            obstacle_info["timestamp"] = time()
-            obstacle_info["label"] = "duckie"
-            obstacle_info["duckieId"] = -1
-            obstacle_info["distance"] = response[4]
-            reply.data = json.dumps(obstacle_info)
-            self.log(reply.data)
+            cases = {1: 'on the oncomming trafic',
+                     2: 'object on the way',
+                     3: 'object in the roadside',
+                     4: 'object is too far',
+                     5: 'emergency stop',
+                     0: 'nothing is on our way'}
+            self.log(response[4])
+            if response[4] == 5:
+                os.system(f'rosparam set /{os.environ["VEHICLE_NAME"]}/kinematics_node/gain 0.0')
+                reply = String()
+                obstacle_info = {}
+                obstacle_info["message"] = "discover_obstacle"
+                obstacle_info["id"] = -1
+                obstacle_info["timestamp"] = time()
+                obstacle_info["label"] = "duckie"
+                obstacle_info["duckieId"] = -1
+                obstacle_info["case"] = "Alarm! Too close. Stopping"
+                reply.data = json.dumps(obstacle_info)
+                self.log(reply.data)
+            elif response[4] != 0:
+                reply = String()
+                obstacle_info = {}
+                obstacle_info["message"] = "discover_obstacle"
+                obstacle_info["id"] = -1
+                obstacle_info["timestamp"] = time()
+                obstacle_info["label"] = "duckie"
+                obstacle_info["duckieId"] = -1
+                obstacle_info["case"] = cases[response[4]]
+                reply.data = json.dumps(obstacle_info)
+                self.log(reply.data)
+            else:
+                os.system(f'rosparam set /{os.environ["VEHICLE_NAME"]}/kinematics_node/gain 1.0')
+                reply = String()
+                obstacle_info = {}
+                obstacle_info["message"] = "nothing's_on_the_way"
+                obstacle_info["id"] = -1
+                obstacle_info["timestamp"] = time()
+                obstacle_info["label"] = "duckie"
+                obstacle_info["duckieId"] = -1
+                obstacle_info["case"] = cases[response[4]]
+                reply.data = json.dumps(obstacle_info)
+                self.log(reply.data)
 
             self._detected_objs.publish(reply)
 
